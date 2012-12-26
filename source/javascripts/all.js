eaf6282133c0b06da3e4074b1e8f78f4b1c66eae
Alex Miles <howdy@alexmilesdesign.com>
2012-12-26 15:44:38 +0100

    Wrote JS to generate the soundwave

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,25 @@
 //= require_tree .
+
+function randomFromTo(from, to){
+	return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+(function($){
+
+	// Global vars
+	var waveLength = 366;
+	var playedLength = 100;
+
+	// Append each single bars to div to create full wave
+	for ( var i = 0; i < waveLength; i++ ) {
+		var rand = randomFromTo(50, 100);
+		var mTop = 100 - rand;
+		$('.wave').append('<div class="bar" style="height:' + rand + 'px; margin-top: ' + mTop + 'px"></div>');
+	}
+
+	// Added played class to bars
+	for ( var p = 0; p < playedLength; p++ ) {
+		$('.bar:eq(' + p + ')').addClass('played');
+	}
+
+})(jQuery);
