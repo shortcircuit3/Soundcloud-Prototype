e20817ac90e57f86c9c2c10401875d7d0c14dbb0
Alex Miles <howdy@alexmilesdesign.com>
2012-12-27 16:14:09 +0100

    Added hover state to play btn

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -10,42 +10,65 @@ function randomFromTo(from, to){
 	var waveLength = 366;
 	var playedLength = 200;
 
-	// Append each single bars to div to create full wave
-	for ( var i = 0; i < waveLength; i++ ) {
-		var rand = randomFromTo(40, 80);
-		var mTop = 80 - rand;
-		$('.wave').append('<div class="bar" id="bar-' + i + '" style="height:' + rand + 'px; margin-top: ' + mTop + 'px"></div>');
-	}
 
-	// Added played class to bars
-	for ( var p = 0; p < playedLength; p++ ) {
-		$('.bar:eq(' + p + ')').addClass('played');
-	}
+	// DOCUMENT.READY()
+	$(document).ready(function() {
+		// Append each single bars to div to create full wave
+		for ( var i = 0; i < waveLength; i++ ) {
+			var rand = randomFromTo(40, 80);
+			var mTop = 80 - rand;
+			$('.wave').append('<div class="bar" id="bar-' + i + '" style="height:' + rand + 'px; margin-top: ' + mTop + 'px"></div>');
+		}
 
-	// Cache the .wave container position
-	var containerPos = $('.wave').offset();
-	
-	// Loop through each image and highlight the relative sound bar showing it has a comment
-	$('.comment-avatars img').each(function(index) {
-		// Find the center of each image
-		var distanceFromLeft = $(this).css('left'); // Get distance from left
-		distanceFromLeft = distanceFromLeft.substring(0, distanceFromLeft.length - 2); // Remove 'px'
-		distanceFromLeft = parseInt(distanceFromLeft, 10); // Convert string to integer
-		avatarCenter = distanceFromLeft + 15; // Find center of avatar
-		// Find relative bar and add class
-		$('.wave').find('.bar').each(function(index){
-			var inputPos = $(this).offset(); // Get the bars position
-			relativeOffset = {
-				left: inputPos.left - containerPos.left // The relative position from the left of .wave
-			};
-			var barDist = relativeOffset.left; // A more sematic name
-			// Find the relative bar (+/- 1px)
-			if( Math.abs(avatarCenter - barDist) <= 1){
-				var id = $(this).attr('id'); // Get the ID
-				$('#' + id).addClass('has-comment'); // Add class '.has-comment' to the relative bar
-			}
+		// Added played class to bars
+		for ( var p = 0; p < playedLength; p++ ) {
+			$('.bar:eq(' + p + ')').addClass('played');
+		}
+
+		// Cache the .wave container position
+		var containerPos = $('.wave').offset();
+		
+		// Loop through each image and highlight the relative sound bar showing it has a comment
+		$('.comment-avatars img').each(function(index) {
+			// Find the center of each image
+			var distanceFromLeft = $(this).css('left'); // Get distance from left
+			distanceFromLeft = distanceFromLeft.substring(0, distanceFromLeft.length - 2); // Remove 'px'
+			distanceFromLeft = parseInt(distanceFromLeft, 10); // Convert string to integer
+			avatarCenter = distanceFromLeft + 15; // Find center of avatar
+			// Find relative bar and add class
+			$('.wave').find('.bar').each(function(index){
+				var inputPos = $(this).offset(); // Get the bars position
+				relativeOffset = {
+					left: inputPos.left - containerPos.left // The relative position from the left of .wave
+				};
+				var barDist = relativeOffset.left; // A more sematic name
+				// Find the relative bar (+/- 1px)
+				if( Math.abs(avatarCenter - barDist) <= 1){
+					var id = $(this).attr('id'); // Get the ID
+					$('#' + id).addClass('has-comment'); // Add class '.has-comment' to the relative bar
+				}
+			});
 		});
 
+		// Return false after submit
+		$('#comment-form').submit(function() {
+			// Do something
+			return false;
+		});
+
+	});
+	
+
+
+	$(window).load(function () {
+		function loadTheRest(){
+			TweenMax.to($('header'), 0.4, {css:{opacity:"1"}, ease:Power4.easeOut });
+			TweenMax.to($('.comment-avatars'), 0.4, {css:{opacity:"1"}, ease:Power4.easeOut });
+			TweenMax.to($('.comment-field'), 0.4, {css:{opacity:"1"}, ease:Power4.easeOut });
+			$('body').removeClass('loading');
+		}
+		TweenMax.to($('.player'), 0.9, {css:{height:"267px"}, ease:Power4.easeOut, onComplete:loadTheRest });
+		
 	});
 
 })(jQuery);
